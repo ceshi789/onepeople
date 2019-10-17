9e261629ffc93b177e06d76403d7a617768a7921
jiang <1173935430@qq.com>
2019-10-17 20:12:28 +0800

    one

---
+++ src/main/java/com/one/oneproject/OneprojectApplication.java
@@ -0,0 +1,13 @@
+package com.one.oneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneprojectApplication.class, args);
+    }
+
+}
